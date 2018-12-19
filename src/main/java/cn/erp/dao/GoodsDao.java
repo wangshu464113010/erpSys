513f85e3ee91940adcf885e20bf14c3bff709651
王澍 <Administrator@hongcheng103>
2018-12-19 19:35:58 +0800

    王澍 2018-12-19 19:35 提交

--- src/main/java/cn/erp/dao/GoodsDao.java
+++ src/main/java/cn/erp/dao/GoodsDao.java
@@ -11,4 +11,8 @@ public interface GoodsDao {
 	public List<Goods> findAll() throws SQLException;
 	//获取最大的商品编号
 	public String getMaxGoodsCode()throws SQLException;
+	public int insert(Goods goods)throws SQLException;
+	public int update(Goods goods)throws SQLException;
+	public int delete(int id)throws SQLException;
+	
 }
--- src/main/java/cn/erp/dao/impl/GoodsDaoImpl.java
+++ src/main/java/cn/erp/dao/impl/GoodsDaoImpl.java
@@ -52,4 +52,32 @@ public class GoodsDaoImpl implements GoodsDao {
 		return query.getCode();
 	}
 
+	@Override
+	public int insert(Goods goods) throws SQLException {
+		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
+		String sql = "insert into t_goods("
+				+ "code,inventory_quantity,min_num,"
+				+ "model,name,producer,purchasing_price"
+				+ ",remarks,selling_price,unit,type_id,state,last_purchasing_price  )"
+				+ " values(?,?,?,?,?,?,?,?,?,?,?,?,?) ";
+		return qr.update(sql);
+	}
+
+	@Override
+	public int update(Goods goods) throws SQLException {
+		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
+		String sql = "update t_goods set "
+				+ " inventory_quantity=?,min_num=?,"
+				+ "model=?,name=?,producer=?,purchasing_price=?"
+				+ ",remarks=?,selling_price=?,unit=?,type_id=? where id = ? ";
+		return qr.update(sql);
+	}
+
+	@Override
+	public int delete(int id) throws SQLException {
+		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
+		String sql = "delete from  t_goods where id = ?";
+		return qr.update(sql,id);
+	}
+
 }
