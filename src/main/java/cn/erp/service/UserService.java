d1372466c7630c821cf1127b5e7e6603ae68088c
yl779085179 <779085179@qq.com>
2018-12-19 18:59:06 +0800

    Revert "1"
    This reverts commit f410c27da1f6674a5f4848befc60fdd0d1daf05a.

--- src/main/java/cn/erp/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package cn.erp.service;
-
-import java.sql.SQLException;
-
-import cn.erp.domain.User;
-
-public interface UserService {
-	User login(String user_name,String password) throws SQLException;
-}
