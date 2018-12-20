e0ec11017f67eea00ddeaf3c72c01b219facb1e8
wangshu <Administrator@hongcheng103>
2018-12-20 10:35:51 +0800

    wangshu

--- src/main/java/com/erp/filter/LoginFilter.java
+++ src/main/java/com/erp/filter/LoginFilter.java
@@ -9,9 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//@WebFilter
 public class LoginFilter implements Filter{
 
 	@Override
